7a2897b7a9c1c546b1400d537a136c4a5df179ba
Rasheed Amir <rasheed@aurorasolutions.io>
2018-10-23 15:49:48 +0200

    add datetime in the menu

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -20,6 +20,7 @@ module.exports = {
                 title: 'Java Backend',
                 children: [
                     '/java-backend/',
+                    '/java-backend/datetime',
                 ]
             },
             {
